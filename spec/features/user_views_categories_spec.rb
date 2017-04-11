706523d91c9509f226e9899f86f01056f7508431
Andy Wooten <andrewdwooten@icloud.com>
2017-04-11 09:07:32 -0600

    Complete category index
    Adds before action to categories controller
    which sets @category to Category.new.
    Builds out feature test to assert count
    of category_instance css selectors.
    All tests currently passing.

--- spec/features/user_views_categories_spec.rb
+++ spec/features/user_views_categories_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 feature 'user visits categories index' do
   scenario 'user views all categories' do
+    VCR.use_cassette('features/categories') do
+      visit '/categories'
 
-    visit '/categories'
+      expect(page).to have_content('Categories')
 
-    expect(page).to have_content('Categories')
+      within('#category_list') do
+        expect(page).to have_selector('#category_instance', count: 15)
+      end
+    end
   end
 end
