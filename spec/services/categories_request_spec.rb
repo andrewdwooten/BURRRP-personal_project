55c89c9cfccb8eb57edc970aea94f192bc05c5e0
Andy Wooten <andrewdwooten@icloud.com>
2017-04-11 08:25:31 -0600

    Add categories method to service
    Adds get_categories method to breweryservice.
    Service spec is currently passing. VCR
    cassette has been recorded. Cassettes added
    to gitignore file.

--- spec/services/categories_request_spec.rb
+++ spec/services/categories_request_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 describe 'BreweryService' do
   it '#get_categories' do
-    db = BreweryService.new
-    categories = JSON.parse(db.get_categories)
+    VCR.use_cassette('brewery_services/categories') do
+      db = BreweryService.new
+      categories = db.get_categories
 
-    expect(categories.class).to eq(Hash)
-
+      first = categories[:data].first
+      expect(categories.class).to eq(Hash)
+      expect(categories[:message]).to eq("Request Successful")
+      expect(categories[:data].count).to eq(15)
+      expect(first[:name]).to eq("British Origin Ales")
+    end
   end
 end
