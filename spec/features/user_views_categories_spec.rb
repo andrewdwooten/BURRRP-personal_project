af52da9f0ee32bab4febdaadd705bf7bcaacb387
Andy Wooten <andrewdwooten@icloud.com>
2017-04-11 09:09:00 -0600

    Add assertion to categories feature test
    Adds assertion to check content of first
    category_instance css. Same is passing.

--- spec/features/user_views_categories_spec.rb
+++ spec/features/user_views_categories_spec.rb
@@ -10,6 +10,10 @@ feature 'user visits categories index' do
       within('#category_list') do
         expect(page).to have_selector('#category_instance', count: 15)
       end
+
+      within all('#category_instance').first do
+        expect(page).to have_content('1: British Origin Ale')
+      end
     end
   end
 end
