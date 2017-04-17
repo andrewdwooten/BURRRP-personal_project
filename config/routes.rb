eeda2dc666ac6152c0e2c2ec257c16248d214458
Andy Wooten <andrewdwooten@icloud.com>
2017-04-17 17:02:40 -0600

    Add API endpoint for brewery search
    Adds api/v1/breweries_search_controller with index method.
    Adds search_by_city method to brewery_service. Method
    returns array of hashes each representing data for one
    brewery. Adds search_by_city method to Brewery PORO
    which get breweries from brewery service by city. Adds
    build_json_breweries method to Brewery PORO.  Method
    returns an array of hashes each containing information
    on one brewery with information necessary for search
    result display. Test of API endpoint is currently passing.
    All tests currently passing with coverage rate of 98.6%.
    Will need to add specific testing for new methods added
    to brewery service adn brewery poro.

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   get '/search', to: 'search#show'
+
+  namespace :api do
+    namespace :v1 do
+      resources :breweries_search, only: [:index]
+    end
+  end
 end
